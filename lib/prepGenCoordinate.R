fb8c94e0523fe6cdd545e74dfbadc7aa979a42c1
afandiadib <adib.abdullah@imm.ox.ac.uk>
2020-02-06 23:07:41 +0000

    update

--- lib/prepGenCoordinate.R
+++ lib/prepGenCoordinate.R
@@ -8,29 +8,40 @@ prepGenCoordinate <- function(env) {
   if (class(env$genomic.coordinate)[1] == "character") {
 
     filename <- env$genomic.coordinate
-    env$genomic.coordinate <- fread(env$genomic.coordinate)
     
-    if (grepl("\\.bed", filename)) {
+    # genomic.coordinate can be replicates
+    env$genomic.coordinate <- lapply(env$genomic.coordinate, fread)
+    
+    if (sum(grepl("\\.bed", filename)) == length(filename)) {
       
-      if (length(colnames(env$genomic.coordinate)) >= 6) {
-        # strand sensitive i.e. has strand information
+      for (i in seq_along(env$genomic.coordinate)) {
         
-        env$genomic.coordinate[, colnames(env$genomic.coordinate)[!colnames(env$genomic.coordinate) %in% 
-                                                            c("V1", "V2", "V3", "V6")] := NULL]
-        
-        setnames(env$genomic.coordinate, c("V1", "V2", "V3", "V6"), c("chromosome", "start", "end", "strand"))
-        
-        # bed use zero-based index and open-end index e.g. [0,10) which means from index 0 until index 9
-        # change to R indexing
-        env$genomic.coordinate[, start := start + 1]
-        
-      } else {
-        # strand insensitive i.e. has no strand information
-        env$genomic.coordinate[, colnames(env$genomic.coordinate)[!colnames(env$genomic.coordinate) %in% 
-                                                            c("V1", "V2", "V3")] := NULL]
-        
-        setnames(env$genomic.coordinate, c("V1", "V2", "V3"), c("chromosome", "start", "end"))
-        env$genomic.coordinate[, strand := "*"]
+        if (length(colnames(env$genomic.coordinate[[i]])) >= 6) {
+          # strand sensitive i.e. has strand information
+          
+          env$genomic.coordinate[[i]][, colnames(env$genomic.coordinate[[i]])[!colnames(env$genomic.coordinate[[i]]) %in% 
+                                                                                c("V1", "V2", "V3", "V6")] := NULL]
+          
+          setnames(env$genomic.coordinate[[i]], c("V1", "V2", "V3", "V6"), c("chromosome", "start", "end", "strand"))
+          
+          # bed use zero-based index and open-end index e.g. [0,10) which means from index 0 until index 9
+          # change to R indexing
+          env$genomic.coordinate[[i]][, start := start + 1]
+          
+        } else {
+          
+          if (strand.mode == "sensitive") {
+            stop("You specify strand sensitive but there is no strand information in the table.")
+          } else {
+            
+            # strand insensitive i.e. has no strand information
+            env$genomic.coordinate[[i]][, colnames(env$genomic.coordinate[[i]])[!colnames(env$genomic.coordinate[[i]]) %in% 
+                                                                                  c("V1", "V2", "V3")] := NULL]
+            
+            setnames(env$genomic.coordinate[[i]], c("V1", "V2", "V3"), c("chromosome", "start", "end"))
+            env$genomic.coordinate[[i]][, strand := "*"]
+          }
+        }
       }
     }
   }
@@ -67,6 +78,9 @@ prepGenCoordinate <- function(env) {
     
     # reassign the consolidated table to genomic.coordinate
     env$genomic.coordinate <- dt
+    
+    # memory copy, so gc() to clear memory
+    gc()
     
     cat("DONE\n")
     
