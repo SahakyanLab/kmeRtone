50b25673c6d31eab3a9eeef1c1fc254ea7044d7d
afandiadib <adib.abdullah@imm.ox.ac.uk>
2020-02-17 12:51:00 +0000

    update

---
+++ lib/countReverseComplementKmers.R
@@ -0,0 +1,33 @@
+countReverseComplement <- function(kmers, env=parent.env()) {
+  # Smartly count the reverse complement sequence from it's contrast strand
+  #
+  # kmers     <string>      A variable name pointing to data.table object. The data.table
+  #                         must contains two named-columns: kmer and count
+  # env     <environment>   An environment object where the data.table exist.
+  
+  # Dependency
+  #     Package   : data.table, stringi
+  #     Function  : reverseComplement
+  
+  if (class(kmers)[1] != "character") {
+    stop("Please input a variable name of kmers table in string format.")
+  }
+  
+  # Smartly count the reverse complement
+  env[[kmers]][, rc := reverseComplement(kmer, form="string")]
+  rc.count <- env[[kmers]][match(rc, kmer), count]
+  
+  # Because some reverse complement sequence are not exist in the plus strand,
+  # match() results in NAs. Change NA to zero
+  rc.count[is.na(rc.count)] <- 0
+  
+  # Update the count
+  env[[kmers]][, count := count + rc.count]
+  
+  # Add the non-existing rc sequence
+  env[[kmers]] <- rbind(env[[kmers]], data.table(kmer = case.kmers[rc.count==0, rc], count = 1), fill=TRUE)
+  
+  # Remove rc column
+  env[[kmers]][, rc := NULL]
+  
+}
