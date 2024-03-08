d56f1ae3ba86d47145979f704f04d8db33d300ee
patrickpflughaupt <patrickpflughaupt@gmail.com>
2024-03-08 11:25:45 +0000

    rm redundant files

--- build.sh
+++
@@ -1,4 +0,0 @@
-rm ../kmeRtone_0.1.tar.gz
-R --slave -e 'Sys.setenv("PKG_CXXFLAGS"="-fopenmp -std=c++11");library(devtools);document();build();install()'
- #-I/project/sahakyanlab/adibabdu/Programs/conda/lib/R/library/stringi/include
-# devtools::install_git("git@github.com:SahakyanLab/kmeRtone.git", ref = "rpackage")
